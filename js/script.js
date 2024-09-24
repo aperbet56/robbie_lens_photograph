ed4550a0e73c1518e9d49416585606195ff81830
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-09-24 17:45:52 +0200

    Création de la fonction lastNameValidation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ const regexEmail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
 const lastName = document.querySelector("#name");
 const email = document.querySelector("#email");
 const textarea = document.querySelector("#message");
+
+/**
+ * Déclaration de la fonction lastNameValidation pour la validation du champ nom
+ *  @param {String} lastName
+ */
+const lastNameValidation = (lastName) => {
+  // Ecoute de l'événement "change" sur l'input lastName
+  lastName.addEventListener("change", (e) => {
+    e.preventDefault();
+    // condition if... else
+    // La méthode match() permet d'obtenir le tableau des correspondances entre la chaîne courante et une expression rationnelle.
+    if (lastName.value.match(regexName)) {
+      document.querySelector("#nameErrorMessage").textContent = " ";
+      return true;
+    } else {
+      document.querySelector("#nameErrorMessage").textContent =
+        "Veuillez saisir un nom valide, ex : Dupont";
+      return false;
+    }
+  });
+};
+// Appel de la fonction lastNameValidation
+lastNameValidation(lastName);
