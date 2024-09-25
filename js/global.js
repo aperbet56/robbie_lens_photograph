42dad30f33c6fd9b9f9cb776059eaa50b250f8d1
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-09-25 16:14:54 +0200

    Ajout de la fonction getCurrentYear

--- js/global.js
+++ js/global.js
@@ -1,5 +1,20 @@
 // Récupération des éléments HTML5
 const arrowBtn = document.querySelector(".arrow__btn");
+const currentYear = document.querySelector(".year");
+
+const getCurrentYear = () => {
+  // Récupération de la date actuelle
+  const date = new Date();
+  //console.log(date);
+
+  // Récupération de l'année
+  const year = date.getFullYear();
+  //console.log(year);
+
+  currentYear.textContent = year;
+};
+// Appel de la fonction getCurrentYear()
+getCurrentYear();
 
 // Ecoute de l'événement "click" sur la flèche
 arrowBtn.addEventListener("click", () => {
